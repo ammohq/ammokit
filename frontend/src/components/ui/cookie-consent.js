432d76c458a07d4b617471d29e32948e29163012
Rense VanderHoek <rense@me.com>
2018-08-16 16:34:39 +0200

    Cookie-consent must load translations through I18n render prop

--- frontend/src/components/ui/cookie-consent.js
+++ frontend/src/components/ui/cookie-consent.js
@@ -1,26 +1,29 @@
 import React, {Component} from 'react';
 import Consent from "react-cookie-consent";
 import {COOKIELAW_COOKIE} from "../../settings";
-import i18n from "../../i18n";
-
+import {I18n} from 'react-i18next';
 
 export default class CookieConsent extends Component {
 
   render() {
     return (
-      <Consent
-        location="bottom"
-        buttonText={i18n.t("cookieConsent.buttons.agree")}
-        cookieName={COOKIELAW_COOKIE}
-        style={{background: "#111111", zIndex: "1100"}}
-        buttonStyle={{
-          color: "#ffffff",
-          background: "#cc7700",
-          fontSize: "12px"
-        }}
-        expires={150}>
-        {i18n.t("cookieConsent.text")}
-      </Consent>
+      <I18n>
+        {(t) => (
+          <Consent
+            location="bottom"
+            buttonText={t("cookieConsent.buttons.agree")}
+            cookieName={COOKIELAW_COOKIE}
+            style={{background: "#111111", zIndex: "1100"}}
+            buttonStyle={{
+              color: "#ffffff",
+              background: "#cc7700",
+              fontSize: "12px"
+            }}
+            expires={150}>
+            {t("cookieConsent.text")}
+          </Consent>
+        )}
+      </I18n>
     )
   }
 }
